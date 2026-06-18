791375354937a7f0dd16b1b432c05dd28179cb9d
f0nt_CODE <font3002@outlook.com>
2026-06-18 20:17:52 -0300

    Arquivos iniciais criados; Estrutura básica criada

---
+++ src/main/java/edu/commerce/daisquina/DaisquinaApplication.java
@@ -0,0 +1,13 @@
+package edu.commerce.daisquina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaisquinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaisquinaApplication.class, args);
+	}
+
+}
